1ae1dbaab3965712010d475d97d5cef1e8fdcda0
Filip Henningsson <erik.filip.henningsson@gmail.com>
2020-02-16 17:54:00 +0100

    Moved intSplit into Util

---
+++ _Util/intSplit.py
@@ -0,0 +1,11 @@
+import math
+
+def intSplit(num):
+    resList=[num]
+    logterm = int(math.log10(num))
+
+    for i in range(logterm,0,-1):
+        tenpot = int(math.pow(10,i))
+        resList.insert(logterm-i,int(resList[-1]/tenpot))
+        resList[logterm-i+1] -= tenpot * int(resList[-1]/tenpot)
+    return resList
