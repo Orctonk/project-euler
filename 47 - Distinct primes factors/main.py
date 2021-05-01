48d2c4c7c005c0665fadae8faa53c708061a895c
Filip Henningsson <c18fhn@cs.umu.se>
2021-05-01 16:54:33 +0200

    Finished problem 47

--- 47 - Distinct primes factors/main.py
+++ 47 - Distinct primes factors/main.py
@@ -1,6 +1,10 @@
 import math
+from functools import cache
 from typing import List
 
+CONSECUTIVE_COUNT = 4
+
+@cache
 def factorize(val):
     if(int(val) != val):
         return None
@@ -22,19 +26,17 @@ def factorize(val):
 
 cur_con = 0
 n = 3
-factorizations = [
-    {},
-    {},
-    {},
-    {}
-]
 
 while True:
     fac = factorize(n)
-    if len(fac) != 4:
+    if len(fac) != CONSECUTIVE_COUNT:
         cur_con = 0
     else:
         cur_con += 1
-        
 
     n += 1
+    
+    if cur_con == CONSECUTIVE_COUNT:
+        break
+
+print(n - cur_con)
